a7790d42b9f999849d42a502eecb68c57097952d
Alweezy <alvinmutisya@gmail.com>
2017-05-12 13:58:09 +0300

    Add events to database

--- src/models/events/events.py
+++ src/models/events/events.py
@@ -1,19 +1,29 @@
+import uuid
+from src.common.database import Database
+from src.common.utils import Utils
+
+
 class Events(object):
     """Creates class event"""
-    def __init__(self, event_id, event_date, calender_id, event_name, event_venue):
+    def __init__(self, event_date, calender_id, event_name, event_venue, _id=None):
         """
         Initialize class event.
-        :param event_id:
+        :param _id:
         :param event_date:
         :param calender_id:
         :param event_name:
         :param event_venue:
         """
-        self.event_id = event_id
+        self._id = uuid.uuid4().hex if None else _id
         self.event_date = event_date
         self.calendar_id = calender_id
-        self.eventname = event_name
+        self.event_name = event_name
         self.event_venue = event_venue
+
+    def save_to_db(self):
+        """Adds event to database
+        """
+        Database.insert('events', self.json())
 
 
 
