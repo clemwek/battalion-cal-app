db651fea61226f5c734e1cf6d3825d940706f4ac
Alweezy <alvinmutisya@gmail.com>
2017-05-12 13:14:30 +0300

    Initial commit

---
+++ src/models/events/events.py
@@ -0,0 +1,22 @@
+class Events(object):
+    """Creates class event"""
+    def __init__(self, event_id, event_date, calender_id, event_name, event_venue):
+        """
+        Initialize class event.
+        :param event_id:
+        :param event_date:
+        :param calender_id:
+        :param event_name:
+        :param event_venue:
+        """
+        self.event_id = event_id
+        self.event_date = event_date
+        self.calendar_id = calender_id
+        self.eventname = event_name
+        self.event_venue = event_venue
+
+
+
+
+
+
